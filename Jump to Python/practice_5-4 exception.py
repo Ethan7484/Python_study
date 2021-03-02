f5609daa113a91c5f742ebdfb08c3140f2cb439b
Ethan7484 <sinji107@gmail.com>
2021-03-02 23:14:57 +0900

    jump to python 5-4 예외처리 완료 commit

--- Jump to Python/practice_5-4 exception.py
+++ Jump to Python/practice_5-4 exception.py
@@ -126,3 +126,123 @@ except (ZeroDivisionError, IndexError) as e:
     print(e)
 """
 # 2개 이상의 오류를 동일하게 처리하기 위해서는 위와 같이 괄호를 사용하여 함께 묶어 처리하면 된다.
+
+"""
+try문에 else절 사용하기
+try문에는 다음처럼 else절을 사용할 수 있다.
+
+try:
+    ...
+except [발생 오류[as 오류 메시지 변수]]:
+    ...
+else:    # 오류가 없을 경우에만 수행된다.
+    ...
+
+try문 수행중 오류가 발생하면 except절이 수행되고 오류가 없으면 else절이 수행된다.
+다음은 try문에 else절을 활용하는 간단한 예이다.
+
+
+try:
+    age = int(input("나이를 입력하세요: "))
+except:
+    print("입력이 정확하지 않습니다.")
+else:
+    if age <= 18:
+        print("미성년자는 출입금지 입니다.")
+    else:
+        print("환영합니다.")
+"""
+# 만약 '나이를 입력하세요:'라는 질문에 숫자가 아닌 다른 값을 입력하면 오류가 발생하여 '입력이 정확하지 않습니다.'라는 문장을 출력한다.
+# 오류가 없을 경우에만 else절이 수행된다.
+
+
+
+### 오류 회피하기
+# 프로그래밍을 하다 보면 특정 오류가 발생할 경우 그냥 통과시켜야 할 때가 있다. 다음 예를 보자.
+"""
+try:
+    f = open("나없는파일", "r")
+except FileNotFoundError:
+    pass
+"""
+# try문 안에서 FileNotFoundError가 발생할 경우에 pass를 사용하여 오류를 그냥 회피하도록 작성한 예제이다.
+
+
+
+### 오류 일부러 발생시키기
+# 이상하게 들리겠지만 프로그래밍을 하다 보면 종종 오류를 일부러 발생시켜야 할 경우도 생긴다. 파이썬은 raise 명령어를 사용해 오류를 강제로 발생시킬 수 있다.
+# 예를 들어 Bird 클래스를 상속받는 자식 클래스는 반드시 fly라는 함수를 구현하도록 만들고 싶은 경우(강제로 그렇게 하고 싶은 경우)가 있을 수 있다. 다음 예를 보자.
+
+class Bird:
+    def fly(Self):
+        raise NotImplementedError
+# 위 예제는 Bird 클래스를 상속 받는 자식 클래스는 반드시 fly 함수를 구현해야 한다는 의지를 보여 준다. 만약 자식 클래스가 fly 함수를 구현하지 않은 상태로  fly 함수를 호출한다면 어떻게 될까?
+# >> NotImplementError는 파이썬 내장 오류로, 꼭 작성해야 하는 부분이 구현되지 않았을 경우 일부러 오류를 일으키기 위해 사용한다.
+"""
+class Eagle(Bird):
+    pass
+eagle = Eagle()
+eagle.fly()
+"""
+# Eagle 클래스는 Bird 클래스를 상속받는다. 그런데 Eagle 클래스에서 fly 함수를 구현하지 않았기 때문에 Bird 클래스의 fly 함수가 호출된다.
+# 그리고 raise문에 의해 NotImplemented Error가 발생할 것이다.
+# >> 상속받는 클래스에서 함수를 재구현하는 것을 메서드 오버라이딩이라고 부른다.
+# NotImplementedError가 발생되지 않게 하려면 다음과 같이 Eagle 클래스에 fly 함수를 반드시 구현해야 한다.
+"""
+class Eagle(Bird):
+    def fly(self):
+        print("very fast")
+
+eagle = Eagle()
+eagle.fly()
+"""
+# 위 예처럼 fly 함수를 구현한 후 프로그램을 실행하면 오류없이 다음 문장이 출력된다.
+# very fast
+
+
+
+### 예외 만들기
+# 프로그램 수행 도중 특수한 경우에만 예외 처리를 하기 위해서 종종 예외를 만들어서 사용한다. 직접 예외를 만들어 보자. 
+# 예외는 다음과 같이 파이썬 내장 클래스인 Exception 클래스를 상속하여 만들 수 있다.
+
+class MyError(Exception):
+    pass
+# 그리고 별명을 출력해 주는 함수를 다음과 같이 작성한다.
+
+def say_nick(nick):
+    if nick == "바보":
+        raise MyError()
+    print(nick)
+# 그리고 다음과 같이 say_nick 함수를 호출해 보자.
+"""
+say_nick("천사")
+say_nick("바보")
+"""
+# 이번에는 예외 처리 기법을 사용하여 MyError 발생을 예외 처리해 보자.
+
+try:
+    say_nick("천사")
+    say_nick("바보")
+except MyError:
+    print("허용되지 않는 별명입니다.")
+# 만약 오류 메시지를 사용하고 싶다면 다음처러 예외 처리를 하면 된다.
+
+try:
+    say_nick("천사")
+    say_nick("바보")
+except MyError as e:
+    print(e)
+# 하지만 프로그램을 실행해 보면 print(e)로 오류 메시지가 출력되지 않는 것을 확인할 수 있다.
+# 오류 메시지를 출력했을 때 오류 메시지가 보이게 하려면 오류 클래스에 다음과 같은 __str__메서드를 구현해야 한다.
+# __str__ 메서드는 print(e)처럼 오류 메시지를 print문으로 출력할 경우에 호출되는 메서드이다.
+
+class MyError(Exception):
+    def __str__(self):
+        return "허용되지 않는 별명입니다."
+
+try:
+    say_nick("천사")
+    say_nick("바보")
+except MyError as e:
+    print(e)
+# 프로그램을 다시 실행해 보면 "허용되지 않는 별명입니다."라는 오류메시지가 출력 되는 것을 확인할 수 있다. 
