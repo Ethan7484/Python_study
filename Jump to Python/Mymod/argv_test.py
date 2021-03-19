54e01f0b6290abf53ae9f7967b4ac0f03263bef1
Ethan7484 <sinji107@gmail.com>
2021-03-19 09:49:25 +0900

    jump to python 5-6 Library
    5-6 라이브러리 주우우우웅간 commit

--- Jump to Python/Mymod/argv_test.py
+++ Jump to Python/Mymod/argv_test.py
@@ -1,4 +1,11 @@
 # argv_test.py
 
 import sys
-print(sys.argv)
+# print(sys.argv)
+
+# print("프로그램 시작")
+# sys.exit('error!!')
+
+# print("실행 안됨")
+
+sys.path
--- Jump to Python/practice_5-6 라이브러리.py
+++ Jump to Python/practice_5-6 라이브러리.py
@@ -40,3 +40,34 @@ D:\Study\Python_study\Jump to Python\Mymod>python argv_test.py you need python
 
 
 # 2. 강제로 스크립트 종료하기 - sys.exit
+"""
+sys.exit()
+"""
+# sys.exit는 Crtl+Z나 Ctrl+D를 눌러서 대화형 인터프리터를 종료하는 것과 같은 기능을 한다. 
+# 프로그램 파일 안에서 사용하면 프로그램을 중단시킨다.
+
+
+# 3. 자신이 만든 모듈 불러와 사용하기 - sys.path
+# sys.path는 파이썬 모듈들이 저장되어 있는 위치를 나타낸다. 즉 이 위치에 있는 파이썬 모듈은 경로에 상관없이 어디에서나 불러올 수 있다.
+# 다음은 그 실행 결과이다.
+"""
+import sys
+sys.path
+[ "d:/pravite/python/Python_study/Jump to Python/practice_5-6 라이브러리.py" ]
+"""
+# 위 예에서 ""는 현재 디렉터리를 말한다. 
+
+"""
+# path_append.py
+import sys
+sys.path.append("d:/pravite/python/Python_study/Jump to Python/Mymod")
+"""
+
+# 위와 같이 파이썬 프로그램 파일에서 sys.path.append를 사용해 경로 이름을 추가할 수 있다. 이렇게 하고 난 후에는 
+# d:/pravite/python/Python_study/Jump to Python/Mymod/ 디렉터리에 있는 파이썬 모듈을 불러와서 사용할 수 있다. 
+
+
+
+### pickle
+# pickle은 객체의 형태를 그대로 유지하면서 파일에 저장하고 불러올 수 있게 하는 모듈이다. 
+# 다음 예는 pickle 모듈의 dump 함수를 사용하여 딕셔너리 객체인 data를 그대로 파일에 저장하는 방법을 보여준다.
