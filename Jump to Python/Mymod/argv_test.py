addd34e16eebfd5cd068fe80cce997aec5bc49fb
Ethan7484 <sinji107@gmail.com>
2021-03-31 23:14:37 +0900

    jump to python 6-5
    탭을 4개의 공백으로 바꾸기 완료 commit

--- Jump to Python/Mymod/argv_test.py
+++ Jump to Python/Mymod/argv_test.py
@@ -8,4 +8,11 @@ import sys
 
 # print("실행 안됨")
 
-sys.path
+import sys
+
+numbers = sys.argv[1:] # 파일 이름을 제외한 명령 행의 모든 입력
+
+result = 0
+for number in numbers:
+    result += int(number)
+print(result)
