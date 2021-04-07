bad56700eb490d20488ae534494fb4c3502f06ed
Ethan7484 <sinji107@gmail.com>
2021-04-07 23:32:09 +0900

    jump to python 6-6
    완료 commit

--- Jump to Python/sub_dir_search.py
+++ Jump to Python/sub_dir_search.py
@@ -5,12 +5,54 @@
 # search("D:/Study/Python_study/Jump to Python/")
 
 
-import sys
+# import os
 
-def search(dirname):
-    filenames = os.listdir(dirname)
-    for filename in filenames:
-        full_filename = os.path.join(dirname, filename)
-        print(full_filename)
+# def search(dirname):
+#     filenames = os.listdir(dirname)
+#     for filename in filenames:
+#         full_filename = os.path.join(dirname, filename)
+#         print(full_filename)
 
-search("D:/Study/Python_study/Jump to Python/")
+# search("D:/Study/Python_study/Jump to Python/")
+
+
+# import os
+
+# def search(dirname):
+#     filenames = os.listdir(dirname)
+#     for filename in filenames:
+#         full_filename = os.path.join(dirname, filename)
+#         ext = os.path.splitext(full_filename)[-1]
+#         if ext == '.py':
+#             print(full_filename)
+
+# search("D:/Study/Python_study/Jump to Python")
+
+# import os
+
+# def search(dirname):
+#     try:
+#         filenames = os.listdir(dirname)
+#         for filename in filenames:
+#             full_filename = os.path.join(dirname, filename)
+#             if os.path.isdir(full_filename):
+#                 search(full_filename)
+#             else:
+#                 ext = os.path.splitext(full_filename)[-1]
+#                 if ext == '.py':
+#                     print(full_filename)
+#     except PermissionError:
+#         pass
+
+# search("D:/Study/Python_study/Jump to Python")
+
+import os
+
+
+
+for (path, dir, files) in os.walk("D:/Study/Python_study/Jump to Python"):
+    for filenmame in files:
+        ext = os.path.splitext(filename)[-1]
+        if ext == '.py':
+            print("%s/%s" % (path, filename))
+
