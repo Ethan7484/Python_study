a0e4d02ca14a13947990406efe733a90f6849052
Ethan7484 <>
2021-05-31 21:28:17 +0900

    commit

--- NC_elice/2-2장_함수와메서드.py
+++ NC_elice/2-2장_함수와메서드.py
@@ -1,1 +1,40 @@
 ###[이론1] 함수
+
+
+# greeting = "밥 먹었어요?"
+# print("서울 :", greeting)
+
+
+# def busan():
+#     greeting = "밥 뭇나?"
+#     print("부산 :", greeting)
+
+
+# busan()
+# print(greeting)
+
+
+# my_list = [1, 2, 3]
+# my_list.append(4)
+# print(my_list)
+# print(my_list.count(2))
+# print(my_list.pop())
+
+# def solve(a, b):
+#     return a * b
+
+# var1 = solve(3, 4)
+# var2 = solve(3, ["Cham"])
+# print(var1)
+# print(var2)
+
+# def our_max(nums):
+#     maxNum = -9999999
+#     for i in nums:
+#         if maxNum < i:
+#             maxNum = i
+#     return maxNum
+
+# print(our_max([1, 2, 10, 9, 3, 7, 0, 99, 28, 85]))
+
+scores = [int(i) for i in input().split()]
